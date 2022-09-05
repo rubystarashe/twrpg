dc6c8adac071f1a416110ab622d2ecbb8bf09a1a
rubystarashe <rubystarashe@gmail.com>
2022-09-05 15:21:36 +0900

    Add check required item on charged

--- components/description.vue
+++ components/description.vue
@@ -4,8 +4,7 @@
   <div class="descarea">
     <div class="desc" v-if="descriptions[hover]" v-html="`<div>`+descriptions[hover]+`</div>`"/>
     <div class="highorder" v-if="recipieslist[hover]">
-      상위 아이템
-      <div class="orderitem" v-for="(d, key) in recipieslist[hover]">{{itemlist[key]}}</div>
+      <div class="orderitem" v-for="(d, key) in recipieslist[hover]" :class="{ target: targetlist[key] }">{{itemlist[key]}}</div>
     </div>
   </div>
 </div>
@@ -13,6 +12,52 @@
 
 <script setup>
 const props = defineProps(['itemlist', 'descriptions', 'hover', 'recipies'])
+const { target } = store('usersetting').toRefs()
+
+const targetlist = computed(() => {
+  const res = {}
+  Object.keys(target.value).forEach(e => {
+    res[e] = true
+    const check = id => {
+      if (!props.recipies[id]) return
+      props.recipies[id].forEach(e => {
+        res[e.item] = true
+        check(e.item)
+      })
+    }
+    check(e)
+  })
+  return res
+})
+
+// const checkisneed = item => {
+//   const list = {}
+
+//   const ready = {}
+//   targetsave.value.items?.forEach(e => {
+//     if (!ready[e]) ready[e] = 0
+//     ready[e]++
+//   })
+
+//   const targets = Object.keys(target.value).map(e => recipies.value[e])
+//   targets.forEach(e => {
+//     if (e) e.forEach(e => {
+//       if (ready[e.item]) return list[e.item] = itemlist.value[e.item]
+      
+//       const check = e => {
+//         if (recipies.value[e.item]) {
+//           recipies.value[e.item].forEach(e => {
+//             if (ready[e.item]) return list[e.item] = itemlist.value[e.item]
+//             check(e)
+//           })
+//         }
+//       }
+//       check(e)
+//     })
+//   })
+//   return list[item]
+// }
+
 
 const mouseposition = ref({
   x: 0,
@@ -69,6 +114,9 @@ onBeforeUnmount(() => {
       .orderitem {
         font-size: .8em;
         margin: 2px;  
+      }
+      .target {
+        background: blue;
       }
     }
   }
