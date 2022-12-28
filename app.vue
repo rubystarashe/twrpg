376720622b777315473646b0bf23aece7f580457
rubystarashe <rubystarashe@gmail.com>
2022-12-28 18:05:15 +0900

    Update app.vue

--- app.vue
+++ app.vue
@@ -138,7 +138,7 @@
 const route = useRoute()
 const lang = ref('ko')
 console.log(route.path)
-if (route.path == '/en') lang.value = 'en'
+if (route.path == '/en/' || route.path == '/en') lang.value = 'en'
 
 const version = '0.62k'
 const translations = {
